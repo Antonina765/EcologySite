e1a2094185397e625c4fb20c86a12b9f4b2bc34a
Antonina765 <antoninakapuza0@gmail.com>
2024-12-02 17:37:40 +0300

    add sql example which in future correct for this project

--- EcologySite/Enums/obj/Debug/net8.0/Enums.AssemblyInfo.cs
+++ EcologySite/Enums/obj/Debug/net8.0/Enums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f62220bc3fedad3da361d6b7405c2dbbbaf060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa26fdb7abbef15f021558ad8bb96b6c71d2a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
